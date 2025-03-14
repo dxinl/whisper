84da977783949b3a9c9fdb2c875d2f95f73e1630
Deng Xinliang <dxinl0307@gmail.com>
2025-03-15 01:20:15 +0800

    beautify codes

--- example.cpp
+++ example.cpp
@@ -6,43 +6,43 @@
 
 class Node1 : public whisper::Node<std::nullptr_t, int> {
 public:
-  whisper::CommonOutput<int> run(std::nullptr_t input) override {
+  whisper::Output<int> run(std::nullptr_t input) override {
     std::cout << "Node1::run" << std::endl;
-    return whisper::CommonOutput<int>{0, "success", 1};
+    return whisper::Output<int>{0, "success", 1};
   }
 };
 
 class Node2 : public whisper::Node<int, std::string> {
 public:
-  whisper::CommonOutput<std::string> run(int input) override {
+  whisper::Output<std::string> run(int input) override {
     std::cout << "Node2::run" << std::endl;
-    return whisper::CommonOutput<std::string>{0, "success",
+    return whisper::Output<std::string>{0, "success",
                                               std::to_string(input)};
   }
 };
 
 class Node3 : public whisper::Node<std::string, std::string> {
 public:
-  whisper::CommonOutput<std::string> run(std::string input) override {
+  whisper::Output<std::string> run(std::string input) override {
     std::cout << "Node3::run" << std::endl;
-    return whisper::CommonOutput<std::string>{0, "success", "Node3_" + input};
+    return whisper::Output<std::string>{0, "success", "Node3_" + input};
   }
 };
 
 class Node4 : public whisper::Node<std::string, int> {
 public:
-  whisper::CommonOutput<int> run(std::string input) override {
+  whisper::Output<int> run(std::string input) override {
     std::cout << "Node4::run" << std::endl;
-    return whisper::CommonOutput<int>{0, "success", 4};
+    return whisper::Output<int>{0, "success", 4};
   }
 };
 
 class Node5 : public whisper::Node<std::tuple<std::string, int>, std::string> {
 public:
-  whisper::CommonOutput<std::string>
+  whisper::Output<std::string>
   run(std::tuple<std::string, int> input) override {
     std::cout << "Node5::run" << std::endl;
-    return whisper::CommonOutput<std::string>{
+    return whisper::Output<std::string>{
         0, "success",
         std::get<0>(input) + ";" + std::to_string(std::get<1>(input))};
   }
